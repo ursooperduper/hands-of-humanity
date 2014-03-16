75857739c8ad277954c30ee14bd0b8f1764ab855
Sarah Kuehnle <sarah@sooperduper.com>
2014-03-16 16:16:31 -0700

    Adds working code for selecting a black card.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source "https://rubygems.org"
 ruby "2.0.0"
 gem 'sinatra', '1.1.0'
+gem 'pg'
 gem 'sequel'
+gem 'sequel_pg', :require=>'sequel'
 gem 'haml'
--- web.rb
+++ web.rb
@@ -1,18 +1,45 @@
 require 'sinatra'
 require 'haml'
+require 'pg'
 require 'sequel'
 
+DB = Sequel.postgres('cards')
 
-get '/' do
-	conn = PG::Connection.open(:dbname => 'cards')
-	b_cards = conn.exec('SELECT id FROM cards WHERE color = \'w\'')
-	w_cards = conn.exec('SELECT id FROM cards WHERE color = \'b\'')
-	
-  
+class Card < Sequel::Model(:cards)
+  def cleanUp
+    cardtext.gsub!(/\[\]/, '_______________')
+    cardtext.gsub!(/\[b\]/, '<br/>')
+    cardtext.gsub!(/\[r\]/, '&reg;')
+    cardtext.gsub!(/\[tm\]/, '&trade;')
+    cardtext.gsub!(/\[i\]/, '<em>')
+    cardtext.gsub!(/\[ii\]/, '</em>')
+    return self
+  end
+end
 
-  @title = "FUH"
 
 
+
+get '/' do	
+  @title = "Hands of Humanity"
+
+  cards = DB.from(:cards)
+
+  # Grab the black and white cards  
+  b_cards = cards.where(:color => 'b')
+  w_cards = cards.where(:color => 'w')
+
+  b_card_list = b_cards.map(:id).shuffle!
+  w_card_list = w_cards.map(:id).shuffle!
+
+  @b_card = Card[b_card_list[0]].cleanUp
+
+  @wh_playcards = []
+  i = 0
+  while i < @b_card.pick
+    @wh_playcards.push(w_card_list[i])
+    i += 1
+  end
 
   haml :random
 end
